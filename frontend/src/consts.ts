f6d781f61ede81b9016458a4ebd6940c45f08b37
Rashaad <rashaadzma@gmail.com>
2025-10-21 00:24:00 +0530

    Initial commit

---
+++ frontend/src/consts.ts
@@ -0,0 +1,9 @@
+export const SITE_TITLE = 'Official Website Of ICDS';
+export const SITE_DESCRIPTION = `The Interact Club of D. S. Senanayake College is the best club in the world&#13;&#10
+
+Anith iskolavala un mokku da bn`;
+
+export const youtube_link = 'https://www.youtube.com/@interactclubofdssc';
+export const twitter_link = 'https://x.com/interact_ds';
+export const instagram_link = 'https://www.instagram.com/ds_interact/';
+export const contact_email = 'rashaadzma@gmail.com';
---
+++ frontend/src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+
+// const blog = defineCollection({
+// 	// Type-check frontmatter using a schema
+// 	schema: z.object({
+// 		title: z.string(),
+// 		description: z.string(),
+// 		// Transform string to Date object
+// 		pubDate: z.coerce.date(),
+// 		updatedDate: z.coerce.date().optional(),
+// 		heroImage: z.string().optional(),
+// 		author: z.string().optional(),
+// 		authorDesignation: z.string().optional(),
+// 		authorImage: z.string().optional(),
+// 	}),
+// });
+
+export const collections = { };
